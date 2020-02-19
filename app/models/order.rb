84164363e78de8632934e161a2ace45bc1cd50cb
Xavi Vila <xavivm@gmail.com>
2020-02-19 13:17:19 +0000

    emaild validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   before_create :set_defaults
 
-  validates :first_name, :country, :postal_code, :email_address, presence: true
+  validates :email_address, presence: true, email: true
+  validates :first_name, :country, :postal_code, presence: true
 
   UNIT_PRICE_CENTS = 299
   CURRENCY = 'USD'.freeze
@@ -24,5 +25,9 @@ class Order < ApplicationRecord
   def next_number
     current = self.class.reorder('number desc').first.try(:number) || '000000000000'
     current.next
+  end
+
+  def validate_email
+    
   end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -5,4 +5,8 @@ class OrderTest < ActiveSupport::TestCase
   should validate_presence_of(:country)
   should validate_presence_of(:postal_code)
   should validate_presence_of(:email_address)
+
+  should_not allow_value('invalid_email').for(:email_address)
+  should_not allow_value('invalid@email').for(:email_address)
+  should_not allow_value('invalid@email com').for(:email_address)
 end
