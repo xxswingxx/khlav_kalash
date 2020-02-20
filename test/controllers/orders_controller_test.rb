d3f3de67b8a4f8550d3f88254496d22393400928
Xavi Vila <xavivm@gmail.com>
2020-02-20 11:22:54 +0000

    improve controller test

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -11,17 +11,53 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    @intent = Order.payment_intent
     get new_order_url
     assert_response :success
   end
 
   test "should create order" do
-    assert_difference('Order.count') do
-      post orders_url, params: { order: { amount_cents: @order.amount_cents, country: @order.country, email_address: @order.email_address, first_name: @order.first_name, last_name: @order.last_name, number: @order.number, postal_code: @order.postal_code } }
+    mock = Minitest::Mock.new
+    def mock.status
+      'succeeded'
+    end
+
+    Stripe::PaymentIntent.stub :retrieve, mock do
+      assert_difference('Order.count') do
+        post orders_url, params: { order: { amount_cents: @order.amount_cents, country: @order.country, email_address: @order.email_address, first_name: @order.first_name, last_name: @order.last_name, number: @order.number, postal_code: @order.postal_code } }
+      end
     end
 
     assert_redirected_to order_permalink_url(Order.last.permalink)
+  end
+
+  test "should not create order if payment did not succeed" do
+    mock = Minitest::Mock.new
+    def mock.status
+      'not yet paid'
+    end
+
+    Stripe::PaymentIntent.stub :retrieve, mock do
+      err = assert_raises(RuntimeError) do
+        post orders_url, params: { order: { amount_cents: @order.amount_cents, country: @order.country, email_address: @order.email_address, first_name: @order.first_name, last_name: @order.last_name, number: @order.number, postal_code: @order.postal_code } }
+      end
+
+      assert_match 'Payment failed', err.message 
+    end
+  end
+
+  test "should not create order if payment already exists" do
+    @order2 = orders(:two)
+
+    mock = Minitest::Mock.new
+    def mock.status
+      'succeeded'
+    end
+
+    Stripe::PaymentIntent.stub :retrieve, mock do
+      err = assert_raises(ActiveRecord::RecordNotUnique) do
+        post orders_url, params: { order: { amount_cents: @order.amount_cents, country: @order.country, email_address: @order.email_address, first_name: @order.first_name, last_name: @order.last_name, number: @order.number, postal_code: @order.postal_code, payment_intent: @order2.payment_intent } }
+      end
+    end
   end
 
   test "should show order" do
